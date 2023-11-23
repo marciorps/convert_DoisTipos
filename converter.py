534b21163ca7c0aa4d16a33a2640bd1e3f60828f
marciorps <marciorps@yahoo.com,.br>
2023-11-23 14:03:16 -0300

    convertendo entre tipos

---
+++ converter.py
@@ -0,0 +1,18 @@
+#conversão de tipos
+idade = input('Qual a sua idade?')
+print(int(idade) > 18) 
+
+print(type(str(5)))
+
+#receber valores decimais
+altura_da_parede = input("Qual a altura da parede?")
+print(float(altura_da_parede) > 2.10)
+
+#conversões mais comuns
+
+int()
+str()
+float()
+list()
+tuple()
+dict()
